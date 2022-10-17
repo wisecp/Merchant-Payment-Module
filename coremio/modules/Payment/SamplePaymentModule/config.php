8084520ab7e3c6ebc633418b3650b08ad4fe81a8
WISECP <info@wisecp.com>
2022-10-17 15:49:16 +0300

    1.0

---
+++ coremio/modules/Payment/SamplePaymentModule/config.php
@@ -0,0 +1,13 @@
+<?php 
+    return [
+        'meta'     => [
+            'name'    => 'Merchant Payment',
+            'version' => '1.0',
+        ],
+        'settings' => [
+            'commission_rate'  => 0,
+            'force_convert_to' => 0,
+            'accepted_countries'    => [],
+            'unaccepted_countries'  => [],
+        ],
+    ];
